bf31ff2dee67a3b2beff38d8f01192d218a34711
wdk <wdk21thu@gmail.com>
2024-04-22 14:00:37 +0800

    update link to fine-tuned model

--- load/shapE-finetuned/download.sh
+++ load/shapE-finetuned/download.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wget https://huggingface.co/datasets/tiange/Cap3D/resolve/main/our_finetuned_models/shapE_finetuned_with_330kdata.pth
+wget https://huggingface.co/datasets/tiange/Cap3D/blob/main/misc/our_finetuned_models/shapE_finetuned_with_330kdata.pth
+
+# if you want to use shapE_finetuned_with_825kdata model, please run the following command
+# wget https://huggingface.co/datasets/tiange/Cap3D/blob/main/misc/our_finetuned_models/shapE_finetuned_with_825kdata.pth
